f05c72ee4485d39a6c2162e65c66751c303c11d8
kceres <perezkceres@gmail.com>
2018-08-10 09:08:36 -0500

    creando el formulario invoice

---
+++ src/app/pages/admin/modules/invoice/components/insert-invoice/insert-invoice.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+
+
+@Component({
+    selector: 'insert-invoice',
+    styleUrls: ['./insert-invoice.component.scss'],
+    templateUrl: './insert-invoice.component.html',
+})
+export class InsertInvoiceComponent implements OnInit, OnDestroy, AfterViewInit {
+
+    constructor() { }
+
+    ngOnInit(): void { }
+
+    ngOnDestroy(): void { }
+
+    ngAfterViewInit(): void { }
+}
--- src/app/pages/admin/modules/invoice/invoice.module.ts
+++ src/app/pages/admin/modules/invoice/invoice.module.ts
@@ -10,12 +10,14 @@ import { BootstrapCoreModule } from '../../../../core/bootstrap.core.module';
 import { AdminInvoiceRoutingModule } from './invoice.routing';
 import { FaqInvoiceComponent } from './components/faq-invoice/faq-invoice.component';
 import { HowToUseComponent } from './components/how-to-use/how-to-use.component';
+import { InsertInvoiceComponent } from './components/insert-invoice/insert-invoice.component';
 
 
 @NgModule({
     declarations: [
         InvoiceComponent,
         MainInvoiceComponent,
+        InsertInvoiceComponent,
         FaqInvoiceComponent,
         HowToUseComponent,
     ],
