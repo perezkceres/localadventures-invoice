7feac1dec017f7d62c476dcebf93530a8704b425
kceres <perezkceres@gmail.com>
2018-08-16 12:58:20 -0500

    integrando animaciones css para abrir y cerrar el modal

--- src/app/pages/admin/modules/invoice/components/how-to-use/how-to-use.component.ts
+++ src/app/pages/admin/modules/invoice/components/how-to-use/how-to-use.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ViewEncapsulation, ElementRef } from '@angular/core';
 
 
 @Component({
@@ -9,11 +9,45 @@ import { Component, OnInit, OnDestroy, AfterViewInit, ViewEncapsulation } from '
 })
 export class HowToUseComponent implements OnInit, OnDestroy, AfterViewInit {
 
-    constructor() { }
+    /** define si esta habilitado el modal */
+    public enableModal: boolean;
+
+    /** activa el control de la salida del modal */
+    public outModal: boolean;
+
+    constructor(private el: ElementRef) { 
+        this.enableModal = false;
+        this.outModal = false;
+    }
 
     ngOnInit(): void { }
 
     ngOnDestroy(): void { }
 
-    ngAfterViewInit(): void { }
+    ngAfterViewInit(): void { 
+        // https://medium.com/claritydesignsystem/four-ways-of-listening-to-dom-events-in-angular-part-1-event-binding-3ec7e9f51a1d
+        // https://stackoverflow.com/questions/37247246/html5-event-handlingonfocus-and-onfocusout-using-angular-2
+
+        // document.getElementsByTagName('input') : to gell all Docuement imputs
+       const inputList = [].slice.call((<HTMLElement>this.el.nativeElement).getElementsByTagName('input'));
+       inputList.forEach((input: HTMLElement) => {
+           input.addEventListener('focus', () => {
+               this.enableModal = true;
+            //    input.classList.toggle('active');
+           });
+        //    input.addEventListener('blur', () => {
+        //        input.removeAttribute('placeholder');
+        //    });
+       });
+    }
+
+    /** oculta el modal */
+    public hideModal() {
+        this.enableModal = false;
+        this.outModal = true;
+
+        setTimeout(() => {
+            this.outModal = false;
+        }, 800);
+    }
 }
